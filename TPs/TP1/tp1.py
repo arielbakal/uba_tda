5c15f1de79995867c2cef9434dd4301c66bd6523
Ariel Bakal <arielbakal.dev@gmail.com>
2024-04-06 01:11:44 -0300

    TP1: code passed judge!!

--- TPs/TP1/tp1.py
+++ TPs/TP1/tp1.py
@@ -10,7 +10,7 @@ for _ in range(T):
     jugadores.append(jugadores_temp)
 
 for caso in range(1, T + 1): # Resolver caso por caso
-    sample = jugadores[caso-1]
+    sample = sorted(jugadores[caso-1])
 
     def formacion_bt(i, cant_jugadores, formacion_actual, suma_actual, suma_max):
         if cant_jugadores == 5:  # Caso base: llegue al límite de solo 5 atacantes
@@ -44,12 +44,35 @@ for caso in range(1, T + 1): # Resolver caso por caso
     suma_max = [float('-inf')]
     formacion_bt(0, 0, [], 0, suma_max) # Backtracking
 
+    def armar_formacion_restante(formacion, sample):
+        formacion_restante = []
+        if len(sample) != len(set(sample)): # Si tengo jugadores repetidos
+            # Diccionarios para contar las ocurrencias de elementos en ambas listas
+            contador_sample = {}
+            contador_formacion = {}
+
+            # Contar las ocurrencias
+            for elemento in sample:
+                contador_sample[elemento] = contador_sample.get(elemento, 0) + 1
+            for elemento in formacion:
+                contador_formacion[elemento] = contador_formacion.get(elemento, 0) + 1
+
+            # Verificar las ocurrencias de cada elemento en sample
+            for elemento, ocurrencias in contador_sample.items():
+                # Si hay más ocurrencias en sample que en formacion, agregarlo a la lista de formacion_restante
+                if ocurrencias > contador_formacion.get(elemento, 0):
+                    formacion_restante.extend([elemento] * (ocurrencias - contador_formacion.get(elemento, 0)))
+        else:
+            formacion_restante = list(filter(lambda jugador: jugador not in formacion, sample))
+
+        return formacion_restante
+
     # Chequeo si hay combinaciones multiples
     # De serlo, decido en base a la suma de las defensas
     if len(atacantes_comb_max) > 1:
         suma_defensa_max = 0
         for combinacion in atacantes_comb_max: # Recorro las combinaciones de atacantes
-            defensores_temp = list(filter(lambda jugador: jugador not in combinacion, sample)) # Armo lista de defensores por combinacion de atacantes
+            defensores_temp = armar_formacion_restante(combinacion, sample) # Armo lista de defensores por combinacion de atacantes
             suma_defensa_temp = sum([jugador[2] for jugador in defensores_temp]) # Sumatoria del valor de defensa de los defensores
             # Verifico si encuentro una o mas defensas que maximicen
             if suma_defensa_temp > suma_defensa_max:
@@ -60,15 +83,11 @@ for caso in range(1, T + 1): # Resolver caso por caso
                 defensores_comb_max.append(defensores_temp)
         defensores_final = min(defensores_comb_max)[0] # De haber varias defensas que maximicen, elijo una por orden alfabetico 
     else: # Unica combinacion de atacantes, simplemente creo lista de defensores
-        defensores_temp = []
-        for jugador in sample:
-            if jugador not in atacantes_comb_max[0]:
-                defensores_temp.append(jugador)
-        defensores_comb_max.append(defensores_temp)
+        defensores_comb_max.append(armar_formacion_restante(atacantes_comb_max[0], sample))
 
     # Defino mi result y las ordeno alfabeticamente
     defensores_final = sorted(defensores_comb_max[0])
-    atacantes_final = sorted(list(filter(lambda jugador: jugador not in defensores_final, sample)))
+    atacantes_final = sorted(armar_formacion_restante(defensores_final, sample))
 
     print(f"Case {caso}:")
     print("(", ", ".join([jugador[0] for jugador in atacantes_final]), ")", sep="")
