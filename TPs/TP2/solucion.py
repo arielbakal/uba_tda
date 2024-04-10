4079a5c30d32419680f0f3b50d183c481d0d2971
Ariel Bakal <arielbakal.dev@gmail.com>
2024-04-10 19:02:24 -0300

    TP2: solucion.py solves inputs individually

--- TPs/TP2/solucion.py
+++ TPs/TP2/solucion.py
@@ -14,20 +14,35 @@ def skyline_incr(i, last_height, total_width):
     if i == N: # Si recorri todo el skyline
         return 0
     
-    if M_incr[i][total_width] == -1:
+    if M_incr[i][total_width] == -1: # Verifico si ya tengo solucion al subproblema
 
-        if heights[0][i] > last_height: # Verifico si el skyline es increasing
+        if heights[0][i] > last_height: # Verifico si el skyline es creciente
 
             # Asigno el maximo entre las recursiones de excluir o incluir el elemento
             M_incr[i][total_width] = max(skyline_incr(i+1, last_height, total_width), skyline_incr(i+1, heights[0][i], total_width + widths[0][i]) + widths[0][i])
 
-        else: # De no ser increasing, sigo recorriendo
+        else: # De no ser creciente, sigo recorriendo
             M_incr[i][total_width] = skyline_incr(i+1, last_height, total_width)    
 
     return M_incr[i][total_width]
 
-N = len(widths[0])
+def skyline_decr(i, last_height, total_width):
+    if i == N: # Si recorri todo el skyline
+        return 0
+    
+    if M_decr[i][total_width] == -1: # Verifico si ya tengo solucion al subproblema
 
+        if heights[0][i] < last_height: # Verifico si el skyline es creciente
+
+            # Asigno el maximo entre las recursiones de excluir o incluir el elemento
+            M_decr[i][total_width] = max(skyline_decr(i+1, last_height, total_width), skyline_decr(i+1, heights[0][i], total_width + widths[0][i]) + widths[0][i])
+
+        else: # De no ser creciente, sigo recorriendo
+            M_decr[i][total_width] = skyline_decr(i+1, last_height, total_width)    
+
+    return M_decr[i][total_width]
+
+N = len(widths[0])
 widths_sum = sum(widths[0])
 
 # Inicializo memoria 
@@ -35,6 +50,7 @@ M_incr = [[-1] * (widths_sum + 1) for _ in range(N + 1)]
 M_decr = [[-1] * (widths_sum + 1) for _ in range(N + 1)]
 
 max_incr_length = skyline_incr(0, float('-inf'), 0)
+max_decr_length = skyline_decr(0, float('+inf'), 0)
 
-print(M_incr)
 print(max_incr_length)
+print(max_decr_length)
