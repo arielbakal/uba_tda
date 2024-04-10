2b27f9a439f3b7c07207cf4a9ed842a2f61b5516
Ariel Bakal <arielbakal.dev@gmail.com>
2024-04-10 19:15:27 -0300

    TP2: solucion.py solves full input, prints cases, solves sample input

--- TPs/TP2/solucion.py
+++ TPs/TP2/solucion.py
@@ -10,47 +10,50 @@ for _ in range(T):
     widths_temp = [int(x) for x in input().split()]
     widths.append(widths_temp)
     
-def skyline_incr(i, last_height, total_width):
+def skyline_incr(i, last_height, total_width, case):
     if i == N: # Si recorri todo el skyline
         return 0
     
     if M_incr[i][total_width] == -1: # Verifico si ya tengo solucion al subproblema
 
-        if heights[0][i] > last_height: # Verifico si el skyline es creciente
+        if heights[case-1][i] > last_height: # Verifico si el skyline es creciente
 
             # Asigno el maximo entre las recursiones de excluir o incluir el elemento
-            M_incr[i][total_width] = max(skyline_incr(i+1, last_height, total_width), skyline_incr(i+1, heights[0][i], total_width + widths[0][i]) + widths[0][i])
+            M_incr[i][total_width] = max(skyline_incr(i+1, last_height, total_width, case), skyline_incr(i+1, heights[case-1][i], total_width + widths[case-1][i], case) + widths[case-1][i])
 
         else: # De no ser creciente, sigo recorriendo
-            M_incr[i][total_width] = skyline_incr(i+1, last_height, total_width)    
+            M_incr[i][total_width] = skyline_incr(i+1, last_height, total_width, case)    
 
     return M_incr[i][total_width]
 
-def skyline_decr(i, last_height, total_width):
-    if i == N: # Si recorri todo el skyline
+def skyline_decr(i, last_height, total_width, case): # Igual que skyline_incr pero distinta comparacion
+    if i == N: 
         return 0
     
     if M_decr[i][total_width] == -1: # Verifico si ya tengo solucion al subproblema
 
-        if heights[0][i] < last_height: # Verifico si el skyline es creciente
+        if heights[case-1][i] < last_height: # Verifico si el skyline es creciente
 
             # Asigno el maximo entre las recursiones de excluir o incluir el elemento
-            M_decr[i][total_width] = max(skyline_decr(i+1, last_height, total_width), skyline_decr(i+1, heights[0][i], total_width + widths[0][i]) + widths[0][i])
+            M_decr[i][total_width] = max(skyline_decr(i+1, last_height, total_width, case), skyline_decr(i+1, heights[case-1][i], total_width + widths[case-1][i], case) + widths[case-1][i])
 
         else: # De no ser creciente, sigo recorriendo
-            M_decr[i][total_width] = skyline_decr(i+1, last_height, total_width)    
+            M_decr[i][total_width] = skyline_decr(i+1, last_height, total_width, case)    
 
     return M_decr[i][total_width]
 
-N = len(widths[0])
-widths_sum = sum(widths[0])
+for case in range(1, T+1):
+    N = len(widths[case-1])
+    widths_sum = sum(widths[case-1])
 
-# Inicializo memoria 
-M_incr = [[-1] * (widths_sum + 1) for _ in range(N + 1)]
-M_decr = [[-1] * (widths_sum + 1) for _ in range(N + 1)]
+    # Inicializo memoria 
+    M_incr = [[-1] * (widths_sum + 1) for _ in range(N + 1)]
+    M_decr = [[-1] * (widths_sum + 1) for _ in range(N + 1)]
 
-max_incr_length = skyline_incr(0, float('-inf'), 0)
-max_decr_length = skyline_decr(0, float('+inf'), 0)
+    max_incr_length = skyline_incr(0, float('-inf'), 0, case)
+    max_decr_length = skyline_decr(0, float('+inf'), 0, case)
 
-print(max_incr_length)
-print(max_decr_length)
+    if max_incr_length >= max_decr_length:
+        print(f"Case {case}. Increasing ({max_incr_length}). Decreasing ({max_decr_length}).")
+    else:
+        print(f"Case {case}. Decreasing ({max_decr_length}). Increasing ({max_incr_length}).")
