bf9402d1088732c3a795bf2860fa29fd15bcaa66
Ariel Bakal <arielbakal.dev@gmail.com>
2024-04-11 18:33:09 -0300

    TP2: solucion_bt.py solves problem with community inputs

--- TPs/TP2/solucion_bt.py
+++ TPs/TP2/solucion_bt.py
@@ -10,25 +10,42 @@ for _ in range(T):
     widths_temp = [int(x) for x in input().split()]
     widths.append(widths_temp)
 
-def skyline_incr_bt(i, last_height, suma_actual):
-    if i == N:
-        if suma_actual >= suma_max[0]:
-            suma_max[0] = suma_actual
-        return
+for case in range(1, T+1):
+    N = len(widths[case-1])
 
-    # Incluir la barra actual si es creciente
-    if heights[0][i] > last_height:
-        skyline_incr_bt(i + 1, heights[0][i], suma_actual + widths[0][i])
+    def skyline_incr_bt(i, last_height, suma_actual, case):
+        if i == N:
+            if suma_actual >= max_incr_length[0]:
+                max_incr_length[0] = suma_actual
+            return
 
-    # Excluir la barra actual y continuar con la siguiente
-    skyline_incr_bt(i + 1, last_height, suma_actual)
+        # Incluir la barra actual si es creciente
+        if heights[case-1][i] > last_height:
+            skyline_incr_bt(i + 1, heights[case-1][i], suma_actual + widths[case-1][i], case)
 
-# Inicio backtracking
-suma_max = [float('-inf')]
-skyline_incr_bt(0, float('-inf'), 0) # Backtracking
-    
-N = len(widths[0])
+        # Excluir la barra actual y continuar con la siguiente
+        skyline_incr_bt(i + 1, last_height, suma_actual, case)
 
-widths_sum = sum(widths[0])
+    def skyline_decr_bt(i, last_height, suma_actual, case):
+        if i == N:
+            if suma_actual >= max_decr_length[0]:
+                max_decr_length[0] = suma_actual
+            return
 
-print(suma_max)
+        # Incluir la barra actual si es creciente
+        if heights[case-1][i] < last_height:
+            skyline_decr_bt(i + 1, heights[case-1][i], suma_actual + widths[case-1][i], case)
+
+        # Excluir la barra actual y continuar con la siguiente
+        skyline_decr_bt(i + 1, last_height, suma_actual, case)
+
+    max_incr_length = [float('-inf')]
+    max_decr_length = [float('-inf')]
+
+    skyline_incr_bt(0, float('-inf'), 0, case)
+    skyline_decr_bt(0, float('+inf'), 0, case)
+
+    if max_incr_length[0] >= max_decr_length[0]:
+        print(f"Case {case}. Increasing ({max_incr_length[0]}). Decreasing ({max_decr_length[0]}).")
+    else:
+        print(f"Case {case}. Decreasing ({max_decr_length[0]}). Increasing ({max_incr_length[0]}).")
