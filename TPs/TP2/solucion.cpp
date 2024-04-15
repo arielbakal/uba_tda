906092db0d481a2f5e29a80a0560b0c74b5b2c8f
arielbakal <arielbakal.dev@gmail.com>
2024-04-15 15:33:42 -0300

    TP2: unnecessary params removed in solucion.cpp

--- TPs/TP2/solucion.cpp
+++ TPs/TP2/solucion.cpp
@@ -7,29 +7,29 @@ vector<vector<int>> heights;
 vector<vector<int>> widths;
 vector<int> buildings_num;
 
-int skyline_incr(int i, int last_height, int total_width, int case_num, int N, vector<vector<int>>& M_incr, const vector<vector<int>>& heights, const vector<vector<int>>& widths) {
+int skyline_incr(int i, int last_height, int total_width, int case_num, int N, vector<vector<int>>& M_incr) {
     if (i == N) 
         return 0;
 
     if (M_incr[i][last_height] == -1) {
-        if (heights[case_num - 1][i] > last_height) {
-            M_incr[i][last_height] = max(skyline_incr(i + 1, last_height, total_width, case_num, N, M_incr, heights, widths), skyline_incr(i + 1, heights[case_num - 1][i], total_width + widths[case_num - 1][i], case_num, N, M_incr, heights, widths) + widths[case_num - 1][i]);
+        if (heights[case_num ][i] > last_height) {
+            M_incr[i][last_height] = max(skyline_incr(i + 1, last_height, total_width, case_num, N, M_incr), skyline_incr(i + 1, heights[case_num][i], total_width + widths[case_num][i], case_num, N, M_incr) + widths[case_num][i]);
         } else {
-            M_incr[i][last_height] = skyline_incr(i + 1, last_height, total_width, case_num, N, M_incr, heights, widths);
+            M_incr[i][last_height] = skyline_incr(i + 1, last_height, total_width, case_num, N, M_incr);
         }
     }
     return M_incr[i][last_height];
 }
 
-int skyline_decr(int i, int last_height, int total_width, int case_num, int N, vector<vector<int>>& M_decr, const vector<vector<int>>& heights, const vector<vector<int>>& widths) {
+int skyline_decr(int i, int last_height, int total_width, int case_num, int N, vector<vector<int>>& M_decr) {
     if (i == N) 
         return 0;
     
     if (M_decr[i][last_height] == -1) {
-        if (heights[case_num - 1][i] < last_height) {
-            M_decr[i][last_height] = max(skyline_decr(i + 1, last_height, total_width, case_num, N, M_decr, heights, widths), skyline_decr(i + 1, heights[case_num - 1][i], total_width + widths[case_num - 1][i], case_num, N, M_decr, heights, widths) + widths[case_num - 1][i]);
+        if (heights[case_num][i] < last_height) {
+            M_decr[i][last_height] = max(skyline_decr(i + 1, last_height, total_width, case_num, N, M_decr), skyline_decr(i + 1, heights[case_num][i], total_width + widths[case_num][i], case_num, N, M_decr) + widths[case_num][i]);
         } else {
-            M_decr[i][last_height] = skyline_decr(i + 1, last_height, total_width, case_num, N, M_decr, heights, widths);
+            M_decr[i][last_height] = skyline_decr(i + 1, last_height, total_width, case_num, N, M_decr);
         }
     }
     return M_decr[i][last_height];
@@ -64,8 +64,8 @@ int main() {
         vector<vector<int>> M_incr(buildings_num[i] + 1, vector<int>(*max_element(heights[i].begin(), heights[i].end()) + 2, -1));
         vector<vector<int>> M_decr(buildings_num[i] + 1, vector<int>(*max_element(heights[i].begin(), heights[i].end()) + 2, -1));
 
-        int max_incr_length = skyline_incr(0, 0, 0, i + 1, buildings_num[i], M_incr, heights, widths);
-        int max_decr_length = skyline_decr(0, *max_element(heights[i].begin(), heights[i].end()) + 1, 0, i + 1, buildings_num[i], M_decr, heights, widths);
+        int max_incr_length = skyline_incr(0, 0, 0, i, buildings_num[i], M_incr);
+        int max_decr_length = skyline_decr(0, *max_element(heights[i].begin(), heights[i].end()) + 1, 0, i, buildings_num[i], M_decr);
 
         if (max_incr_length >= max_decr_length) {
             cout << "Case " << i + 1 << ". Increasing (" << max_incr_length << "). Decreasing (" << max_decr_length << ")." << endl;
