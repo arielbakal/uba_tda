e979dcb1324f6f37fdc908d9f3f442de10f17ce5
arielbakal <arielbakal.dev@gmail.com>
2024-04-17 20:25:54 -0300

    TP3: sol.cpp M initializated & first aproximation of acorn_pd

--- TPs/TP3/solucion.cpp
+++ TPs/TP3/solucion.cpp
@@ -13,6 +13,38 @@ struct Forest {
 vector<Forest> forest_features; // contains (t, h, f) for each forest
 vector<vector<vector<int>>> forest; // contains acorns heights for each tree for each forest (forest<tree<acorn>>)
 
+int acorn_pd(int t, int h, int f, int c, vector<vector<int>> M, bool first_call) {
+
+    if (first_call) {
+        
+        for (int tree=0; tree<forest_features[c].t; tree++) { // i have t-bottoms
+
+            if (h == forest_features[c].h + 1) { // base case (got to the top)
+                return 0;
+            }
+
+            if (M[t][h] == -1){
+
+                M[t][h] = max() 
+
+            }
+
+        }
+
+    } else {
+
+
+
+    }
+
+    
+
+}
+
+
+
+
+
 int main() {
     int C;
     cin >> C;
@@ -50,19 +82,29 @@ int main() {
         int zero; cin >> zero;
     }
 
-    // Output forest_features
-    for (const auto& feature : forest_features) {
-        cout << feature.t << " " << feature.h << " " << feature.f << endl;
-    }
+    // // Output forest_features
+    // for (const auto& feature : forest_features) {
+    //     cout << feature.t << " " << feature.h << " " << feature.f << endl;
+    // }
 
-    // Output forest
-    for (const auto& trees : forest) {
-        for (const auto& tree : trees) {
-            for (int acorn : tree) {
-                cout << acorn << " ";
-            }
-            cout << endl;
-        }
+    // // Output forest
+    // for (const auto& trees : forest) {
+    //     for (const auto& tree : trees) {
+    //         for (int acorn : tree) {
+    //             cout << acorn << " ";
+    //         }
+    //         cout << endl;
+    //     }
+    // }
+
+    for (int c=0; c<C; c++) {
+
+        vector<vector<int>> M(forest_features[c].t + 1, forest_features[c].h + 1, -1);
+
+        max_acorns = acorn_pd(forest_features[c].t, forest_features[c].h, forest_features[c].f, M);
+
+        cout << max_acorns;
+
     }
 
     return 0;
