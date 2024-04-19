d205fbf1a9b755dd876257e1b9ffe917967f9ab5
Ariel Bakal <arielbakal.dev@gmail.com>
2024-04-19 20:55:51 -0300

    TP3: sol.cpp shorter version

--- TPs/TP3/solucion.cpp
+++ TPs/TP3/solucion.cpp
@@ -10,83 +10,52 @@ struct Forest {
     int t, h, f;
 };
 
+int acorns_at_height(int t, int h, int c);
+
 vector<Forest> forest_features; // contains (t, h, f) for each forest
 vector<vector<vector<int>>> forest; // contains acorns heights for each tree for each forest (forest<tree<acorn>>)
 
-int acorn_pd(int t, int h, int total_acorns, int f, int c, vector<vector<int>> M, bool first_call) {
-
-    if (first_call) { 
-        
-        for (int tree=0; tree<forest_features[c].t; tree++) { // i have t-bottoms
-
-            if (h >= forest_features[c].h + 1) { // base case (got to the top)
-                return 0;
-            }
-
-            if (M[tree][h] == -1){
-
-                int continue_at_tree = acorn_pd(t, h+1, total_acorns + acorn_at_height(t, h, c), f, c, M, false);
-
-                int other_trees_max = 0;
-                for (int other_tree = 0; other_tree < forest_features[c].t; other_tree++) {
-                    if (other_tree != tree) {
-                        // Move to other tree, decrease height by 2
-                        int acorn_from_other_tree = acorn_pd(other_tree, h+f, total_acorns + acorn_at_height(other_tree, h, c), f, c, M, false);
-                        other_trees_max = max(other_trees_max, acorn_from_other_tree);
-                    }
-                }
-
-                M[tree][h] = max(continue_at_tree, other_trees_max);
-            }
-
-            return M[t][h];
-        }
-
-    } else {
-
-        if (h >= forest_features[c].h + 1) { // base case (got to the top)
-            return 0;
-        }
-
-        if (M[t][h] == -1) {
-
-            int continue_at_tree = acorn_pd(t, h+1, total_acorns + acorn_at_height(t, h, c), f, c, M, false);
-
-            int other_trees_max = 0;
-            for (int other_tree = 0; other_tree < forest_features[c].t; other_tree++) {
-                if (other_tree != t) {
-                    // Move to other tree, decrease height by 2
-                    int acorn_from_other_tree = acorn_pd(other_tree, h+f, total_acorns + acorn_at_height(other_tree, h, c), f, c, M, false);
-                    other_trees_max = max(other_trees_max, acorn_from_other_tree);
-                }
-            }
-
-            M[t][h] = max(continue_at_tree, other_trees_max);
-
-        }
-
-        return M[t][h];
-    }
-
-    return M[t][h];
-};
-
-int acorn_at_height(int t, int h, int c) {
+int acorns_at_height(int t, int h, int c) {
 
     vector<int> acorns_at_tree = forest[c][t];
 
-    int acorn_at_height = 0;
+    int acorns = 0;
 
     for (int acorn : acorns_at_tree) {
         if (acorn == h) {
-            acorn_at_height += 1;
+            acorns += 1;
         }
     }
 
-    return acorn_at_height;
+    return acorns;
 
 };
 
+int acorn_pd(int t, int h, int f, int c, vector<vector<int>>& M) {
+
+    if (h > forest_features[c].h) { // base case (got to the top)
+        return 0;
+    }
+
+    if (M[t][h] == -1) {
+
+        int continue_at_tree = acorns_at_height(t, h, c) + acorn_pd(t, h+1, f, c, M);
+
+        int other_trees_max = 0;
+        for (int other_tree = 0; other_tree < forest_features[c].t; other_tree++) {
+            if (other_tree != t) {
+                // Move to other tree, decrease height by 2
+                int acorn_from_other_tree = acorns_at_height(other_tree, h, c) + acorn_pd(other_tree, h+f, f, c, M);
+                other_trees_max = max(other_trees_max, acorn_from_other_tree);
+            }
+        }
+
+        M[t][h] = max(continue_at_tree, other_trees_max);
+
+    } 
+
+    return M[t][h];
+};
 
 int main() {
     int C;
@@ -140,15 +109,15 @@ int main() {
     //     }
     // }
 
-    for (int c=0; c<C; c++) {
+    // for (int c=0; c<C; c++) {
 
-        vector<vector<int>> M(forest_features[c].t + 1, vector<int>(forest_features[c].h + 1, -1));
+    //     vector<vector<int>> M(forest_features[c].t + 1, vector<int>(forest_features[c].h + 1, -1));
 
-        int max_acorns = acorn_pd(forest_features[c].t, forest_features[c].h, 0, forest_features[c].f, c, M, true);
+    //     int max_acorns_tree1 = acorn_pd(1, 1, forest_features[c].f, c, M);
 
-        cout << max_acorns << endl;
+    //     cout << max_acorns_tree1 << endl;
 
-    }
+    // }
 
     return 0;
 }
