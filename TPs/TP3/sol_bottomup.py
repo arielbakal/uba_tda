68aef4074e4dea6c93fb05d698c49ad4db0ebb1d
Ariel Bakal <arielbakal.dev@gmail.com>
2024-04-23 02:04:33 -0300

    TP3: sol_bottomup.py cleaned

--- TPs/TP3/sol_bottomup.py
+++ TPs/TP3/sol_bottomup.py
@@ -1,47 +1,35 @@
-# NOTES
-# i'll probably need to save in each tree<acorn> vector the amount of acorns at first position, 
-# to know how many iteration i've to do, also to get O(1) accesing that var
-
-import sys
-
-# Set the recursion limit to a higher value
-sys.setrecursionlimit(2100)  # Set the limit to 10,000 recursions
-
 class Forest:
     def __init__(self, t, h, f):
         self.t = t
         self.h = h
         self.f = f
 
-def acorns_at_height(t, h, c):
-    acorns_at_tree = forest[c][t]
-    acorns = 0
-    for acorn in acorns_at_tree:
-        if acorn == h:
-            acorns += 1
-    return acorns
+def acorn_pd(c):
+    t, h, f = forest_features[c].t, forest_features[c].h, forest_features[c].f
 
-def acorn_pd(f, c):
-    M = [[0] * (forest_features[c].h + 1) for _ in range(forest_features[c].t)]
-    M_max = [0] * (forest_features[c].h + 1)
+    M = [[0] * (h + 1) for _ in range(t)] # memo matrix
+    # max at height list, for getting the acorns of the tree that maximices
+    M_max = [0] * (h + 1) 
 
-    for tree in range(forest_features[c].t):
+    # Initialize acorns 
+    for tree in range(t):
         for acorn_height in forest[c][tree]:
             M[tree][acorn_height] += 1
 
     # Iterate bottom-up
-    for height in range(1, forest_features[c].h + 1):
-        for tree in range(forest_features[c].t):
-            if height - f > 0:
-                M[tree][height] = max(M[tree][height] + M[tree][height-1], M[tree][height] + M_max[height-f])
-            else:
+    for height in range(1, h + 1):
+        for tree in range(t):
+            if height - f > 0: # Verify if i can fly to other tree
+                continue_at_tree = M[tree][height] + M[tree][height-1]
+                fly_other_tree = M[tree][height] + M_max[height-f]
+                M[tree][height] = max(continue_at_tree, fly_other_tree)
+            else: # else, continue
                 M[tree][height] += M[tree][height-1]
+            
+            # update max acorns at height
             M_max[height] = max(M_max[height], M[tree][height])
 
     max_acorns = max(M_max)
-
-    # print(M)
-    # print(M_max)
 
     return max_acorns
 
@@ -68,5 +56,5 @@ for _ in range(C):
 zero = int(input())
 
 for c in range(C):
-    max_acorns = acorn_pd(forest_features[c].f, c)
+    max_acorns = acorn_pd(c)
     print(max_acorns)
