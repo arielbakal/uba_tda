8aa2b6285bd1062f6f2239faaafe34bf9df2c3c8
Ariel Bakal <arielbakal.dev@gmail.com>
2024-04-26 14:57:43 -0300

    TP4: sol.cpp accepted!

--- TPs/TP4/solucion.cpp
+++ TPs/TP4/solucion.cpp
@@ -2,41 +2,40 @@
 #include <string>
 using namespace std;
 
-bool are_equivalent(const string& string_a, const string& string_b) {
-    int len_a = string_a.length();
-    int len_b = string_b.length();
+bool are_equivalent(const string& string_a, const string& string_b, int start_a, int end_a, int start_b, int end_b) {
+    int len_a = end_a - start_a + 1;
+    int len_b = end_b - start_b + 1;
 
-    if (len_a != len_b) // Error handling
-        return false;
-    
-    if (len_a == 0)
+    // Error handling
+    if (len_a != len_b || len_a == 0)
         return false;
 
-    if (string_a == string_b) // Base case (they are equal)
+    // Base case (they are equal)
+    if (string_a.compare(start_a, len_a, string_b, start_b, len_b) == 0)
         return true;
-    
+
+    // Early return if the strings are not equal
     if (len_a == 1)
         return false;
-    
-    if (len_a % 2 == 0) { // If even length
+
+    // If even length
+    if (len_a % 2 == 0) {
         int mid = len_a / 2;
 
-        string string_a_1 = string_a.substr(0, mid);
-        string string_b_1 = string_b.substr(0, mid);
-
-        bool first_case_1 = are_equivalent(string_a_1, string_b_1);
+        // Check if first halves are equivalent
+        bool first_case_1 = are_equivalent(string_a, string_b, start_a, start_a + mid - 1, start_b, start_b + mid - 1);
         if (first_case_1) {
-            bool first_case_2 = are_equivalent(string_a.substr(mid), string_b.substr(mid));
-            if (first_case_2) {
+            // Check if second halves are equivalent
+            bool first_case_2 = are_equivalent(string_a, string_b, start_a + mid, end_a, start_b + mid, end_b);
+            if (first_case_2)
                 return true;
-            }
         } else {
-            bool second_case_1 = are_equivalent(string_a_1, string_b.substr(mid));
+            // Check if the halves are equivalent after swapping
+            bool second_case_1 = are_equivalent(string_a, string_b, start_a, start_a + mid - 1, start_b + mid, end_b);
             if (second_case_1) {
-                bool second_case_2 = are_equivalent(string_a.substr(mid), string_b_1);
-                if (second_case_2) {
+                bool second_case_2 = are_equivalent(string_a, string_b, start_a + mid, end_a, start_b, start_b + mid - 1);
+                if (second_case_2)
                     return true;
-                }
             }
         }
         return false;
@@ -48,12 +47,12 @@ bool are_equivalent(const string& string_a, const string& string_b) {
 int main() {
     string string_a, string_b;
     cin >> string_a >> string_b;
-    
-    if (are_equivalent(string_a, string_b)) {
+
+    if (are_equivalent(string_a, string_b, 0, string_a.length() - 1, 0, string_b.length() - 1)) {
         cout << "YES";
     } else {
         cout << "NO";
     }
 
     return 0;
-}   
+}
