cdf366c53421e7524b8f18eeee7e4cd02b4a6b29
arielbakal <arielbakal.dev@gmail.com>
2024-05-02 17:04:12 -0300

    TP6: sol.cpp TLE on test 6

--- TPs/TP6/solucion.cpp
+++ TPs/TP6/solucion.cpp
@@ -4,28 +4,44 @@
 #include <tuple>
 using namespace std;
 
+// NOTES: remove pair before swapping
+
 int main() {
     
     int n; cin >> n;
     string string_s; cin >> string_s;
     string string_t; cin >> string_t;
 
-    char* s = &string_s[0];
-    char* t = &string_t[0];
+    vector<tuple<char, int>> s;
+    vector<tuple<char, int>> t;
+    int string_len = 0;
+
+    // remove all equal pairs (there's no need for swapping)
+    for (int i=0; i<n; i++) {
+        if (string_s[i] != string_t[i]) {
+            s.push_back(make_tuple(string_s[i],i));
+            t.push_back(make_tuple(string_t[i],i));
+            string_len++;
+        }
+    }
 
     vector<tuple<int, int>> swaps_memo;
 
     int ops_sum = 0;
 
     auto swap = [&](int s_index, int t_index) {
-        char temp_s = s[s_index];
-        s[s_index] = t[t_index];
-        t[t_index] = temp_s;
+        // swap chars, maintaining index
+        tuple<char, int> new_s = make_tuple(get<0>(t[t_index]), get<1>(s[s_index]));
+        tuple<char, int> new_t = make_tuple(get<0>(s[s_index]), get<1>(t[t_index]));
+        s[s_index] = new_s;
+        t[t_index] = new_t;
+
+        swaps_memo.push_back(make_tuple(get<1>(s[s_index]), get<1>(t[t_index])));
     };
 
-    for (int i=0; i<n; i++) { // iterate over s
+    for (int i=0; i<string_len; i++) { // iterate over s
 
-        if (s[i] != t[i]) { // if a get diff pair
+        if (get<0>(s[i]) != get<0>(t[i])) { // if a get diff pair
 
             if (n == 1) {
                 ops_sum = -1;
@@ -33,39 +49,36 @@ int main() {
 
             bool found_equal_pair = false;
 
-            for (int j=i+1; j<n; j++) { // iterate over {i...n} and search for second diff pair
+            for (int j=i+1; j<string_len; j++) { // iterate over {i...n} and search for second diff pair
 
-                if (s[j] != t[j]) { // got second diff pair
+                if (get<0>(s[j]) != get<0>(t[j])) { // got second diff pair
 
-                    if (s[i] == s[j]) { // got equal pairs
+                    if (get<0>(s[i]) == get<0>(s[j])) { // got equal pairs
                         // swap s[i] with t[j]
                         swap(i, j);
-                        swaps_memo.push_back(make_tuple(i, j));
                         ops_sum++;
 
                         found_equal_pair = true;
-                        j = n; // end loop
-                        // i = 0; // restart primary loop
+                        j = string_len; // end loop
+                        i = 0; // restart primary loop
                     } 
                 }
                 
             }
 
             if (found_equal_pair == false) { // find second diff pair, not equal
-                for (int k=i+1; k<n; k++) {
+                for (int k=i+1; k<string_len; k++) {
 
-                    if (s[k] != t[k]) { // got second diff pair (here only handles not equal diff pairs)
+                    if (get<0>(s[k]) != get<0>(t[k])) { // got second diff pair (here only handles not equal diff pairs)
                         // swap s[k] with t[k]
                         swap(k, k);
-                        swaps_memo.push_back(make_tuple(k, k));
                         ops_sum++;
                         // then swap s[k] with t[i] 
                         swap(k, i);
-                        swaps_memo.push_back(make_tuple(k, i));
                         ops_sum++;
 
-                        k = n; // end loop
-                        // i = 0; // restart primary loop
+                        k = string_len; // end loop
+                        i = 0; // restart primary loop
                     }
                     if (k == n-1) { //if didnt found second diff pair, then there is no solution
                         ops_sum = -1;
@@ -73,6 +86,8 @@ int main() {
                 }
             }
             found_equal_pair = false; // restart var for next pairs
+        } else {
+
         }
     }
 
