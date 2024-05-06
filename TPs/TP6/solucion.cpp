e3099d769ace444e962512e1a1ac2c20495978ee
arielbakal <arielbakal.dev@gmail.com>
2024-05-06 00:13:35 -0300

    TP6: sol.cpp cleaner

--- TPs/TP6/solucion.cpp
+++ TPs/TP6/solucion.cpp
@@ -28,71 +28,56 @@ int main() {
         s[s_index] = t[t_index];
         t[t_index] = temp_s;
         swaps_memo.push_back({s_index, t_index});
+        ops_sum++;
     };
     
     bool no_solution = false;
 
     if (diff_pairs % 2 == 0) {
-        if (s.length() == t.length()) {
-            for (int i=0; i<n; i++) { // iterate over s
-        
-            
-                if (no_solution) {
-                    break;
-                }
-        
-                if (s[i] != t[i]) { // if a get diff pair
-        
-                    if (n == 1) {
-                        ops_sum = -1;
-                        no_solution = true;
-                        break;
-                    }
-        
-                    bool found_equal_pair = false;
-        
-                    for (int j=i+1; j<n; j++) { // iterate over {i...n} and search for second diff pair
-        
-                        if (s[j] != t[j]) { // got second diff pair
-        
-                            if (s[i] == s[j]) { // got equal pairs
-                                // swap s[i] with t[j]
-                                swap(i, j);
-                                ops_sum++;
-        
-                                found_equal_pair = true;
-                                break;
-                            } 
-                        }
-                        
-                    }
-        
-                    if (found_equal_pair == false) { // find second diff pair, not equal
-                        for (int k=i+1; k<n; k++) {
-        
-                            if (s[k] != t[k]) { // got second diff pair (here only handles not equal diff pairs)
-                                // swap s[k] with t[k]
-                                swap(k, k);
-                                ops_sum++;
-                                // then swap s[k] with t[i] 
-                                swap(k, i);
-                                ops_sum++;
-        
-                                break;
-                            }
-                            if (k == n-1) { //if didnt found second diff pair, then there is no solution
-                                ops_sum = -1;
-                                no_solution = true;
-                                break;
-                            }
-                        }
-                    }
-                    found_equal_pair = false; // restart var for next pairs
-                }
+        for (int i=0; i<n; i++) { // iterate over s
+    
+            if (no_solution) {
+                break;
             }
-        } else {
-            no_solution = true;
-            ops_sum = -1;
+    
+            if (s[i] != t[i]) { // if a get diff pair
+    
+                bool found_equal_pair = false;
+    
+                for (int j=i+1; j<n; j++) { // iterate over {i...n} and search for second diff pair
+    
+                    if (s[j] != t[j]) { // got second diff pair
+    
+                        if (s[i] == s[j]) { // got equal pairs
+                            // swap s[i] with t[j]
+                            swap(i, j);
+    
+                            found_equal_pair = true;
+                            break;
+                        } 
+                    }
+                    
+                }
+    
+                if (found_equal_pair == false) { // find second diff pair, not equal
+                    for (int k=i+1; k<n; k++) {
+    
+                        if (s[k] != t[k]) { // got second diff pair (here only handles not equal diff pairs)
+                            // swap s[k] with t[k]
+                            swap(k, k);
+                            // then swap s[k] with t[i] 
+                            swap(k, i);
+                            break;
+                        }
+                        if (k == n-1) { //if didnt found second diff pair, then there is no solution
+                            ops_sum = -1;
+                            no_solution = true;
+                            break;
+                        }
+                    }
+                }
+                found_equal_pair = false; // restart var for next pairs
+            }
         }
     } else {
         no_solution = true;
