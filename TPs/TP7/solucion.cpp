70ff26189d4540eba69e11e92964f5fdead5d59c
Ariel Bakal <arielbakal.dev@gmail.com>
2024-05-06 23:12:34 -0300

    TP7: sol.cpp created and receives inputs

---
+++ TPs/TP7/solucion.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// NOTES: 
+// since i need to get neighbors constatly, i opt for representing graph as an adjacency list
+// 2nd graph is crearly a digraph. Again, i need to get neighbors constantly -> adjacency list
+
+// there is no graph as tree, cant use DFS or BFS. Look for Djikstra or A*
+
+int main() {
+
+    int r, d, s;
+    int blanck;
+
+    while (r != 0 && d != r, && s != r) {
+
+        cin >> r >> d >> s;
+
+        for (int i=0; i<d; i++) {
+
+        }
+
+        for (int j=0; j<s; j++) {
+
+        }
+
+        cin >> blanck;
+    }
+
+
+    return 0;
+}
