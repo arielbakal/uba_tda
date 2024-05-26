121c6fd29b85dff24dfe78689a0dbc24c79246e5
Ariel Bakal <arielbakal.dev@gmail.com>
2024-05-26 16:37:49 -0300

    TP9: sol.py read inputs

--- TPs/TP9/sol.py
+++ TPs/TP9/sol.py
@@ -1,1 +1,14 @@
 # Notes: impl graph and priorityqueue  
+
+T = int(input()) # test cases
+keys_global = []
+
+for _ in range(T):
+    safe = input().split()
+    N = int(safe[0])
+    keys = [safe[i] for i in range(1, N+1)]
+    keys = [tuple(int(d) for d in key) for key in keys] 
+    keys_global.append(keys)
+    
+
+
