0d7ec5accd48e3b8351303b7b898530102f84251
arielbakal <arielbakal.dev@gmail.com>
2024-05-29 14:52:41 -0300

    TP9: sol.cpp accepted!!

--- TPs/TP9/sol.cpp
+++ TPs/TP9/sol.cpp
@@ -6,8 +6,6 @@
 #include <unordered_map>
 using namespace std;
 
-// NOTES: solves but TLE
-
 vector<string> keys;
 
 int single_key_dist(int a, int b) {
@@ -28,55 +26,54 @@ int keys_dist(string key1, string key2) {
     return distance;
 }
 
-pair<int, string> min_start_cost(vector<string> keys, int N) {
+pair<int, int> min_start_cost(vector<string> keys, int N) {
     int min_dist = 26;
-    string min_key;
+    int min_key;
     string init = "0000";
-    for (string key: keys) {
-        int dist = keys_dist(init, key);
+    for (int i=0; i<N; i++) {
+        int dist = keys_dist(init, keys[i]);
         if (dist < min_dist) {
-            min_key = key;
+            min_key = i;
             min_dist = dist;
         }
     }
-    return pair<int, string>({min_dist, min_key});
+    return pair<int, int>({min_dist, min_key});
 }
 
-unordered_map<string, vector<pair<string, int>>> build_graph(vector<string> keys, int N) {
-    unordered_map<string, vector<pair<string, int>>> adj_list;
-    for (const auto& key1: keys) {
-        for (const auto& key2: keys) {
-            if (key1 != key2) {
-                adj_list[key1].push_back({key2, keys_dist(key1, key2)});
+unordered_map<int, vector<pair<int, int>>> build_graph(vector<string> keys, int N) {
+    unordered_map<int, vector<pair<int, int>>> adj_list;
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) {
+            if (i != j) {
+                adj_list[i].push_back({j, keys_dist(keys[i], keys[j])});
             }
         }
     }
     return adj_list;
 }
 
-int prim_min_cost(unordered_map<string, vector<pair<string, int>>> graph, int starting_cost, string starting_key, int N) {
+int prim_min_cost(unordered_map<int, vector<pair<int, int>>> graph, int starting_cost, int starting_key, int N) {
     int total_cost = starting_cost;
-    unordered_map<string, bool> visited;
+    vector<bool> visited(N, false);
     visited[starting_key] = true;
 
-    for (int i=0; i<N; i++) {
+    for (int k=0; k<N; k++) {
         int min_dist = 26;
-        string min_key = "-1";
-        for (const auto& node: graph) {
-            string key = node.first;
-            if (!(visited[key])) { 
-                vector<pair<string, int>> neighbors = graph[key];
-                for (pair<string, int> neighbor: neighbors) {
+        int min_key = -1;
+        for (int i=0; i<N; i++) {
+            if (!(visited[i])) { 
+                vector<pair<int, int>> neighbors = graph[i];
+                for (pair<int, int> neighbor: neighbors) {
                     if (visited[neighbor.first]) { 
                         if (neighbor.second < min_dist) {
                             min_dist = neighbor.second;
-                            min_key = key;
+                            min_key = i;
                         }
                     }
                 }
             }
         }
-        if (min_key == "-1") {
+        if (min_key == -1) {
             return total_cost;
         }
         visited[min_key] = true;
@@ -97,9 +94,9 @@ int main() {
             cin >> keys[i]; 
         }   
         
-        unordered_map<string, vector<pair<string, int>>> graph = build_graph(keys, N);
+        unordered_map<int, vector<pair<int, int>>> graph = build_graph(keys, N);
 
-        pair<int, string> start = min_start_cost(keys, N);
+        pair<int, int> start = min_start_cost(keys, N);
 
         int min_cost = prim_min_cost(graph, start.first, start.second, N);
         
