96b6730bcb0c69319fca0044d305ef8d98e88d5e
arielbakal <arielbakal.dev@gmail.com>
2024-06-01 21:16:52 -0300

    TP10: sol.cpp reads single input

---
+++ TPs/TP10/sol.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    while (true) {
+        int n; // n elevators
+        int k; // objective k floor
+        cin >> n >> k;
+
+        vector<int> T(n); // each elevator's time between adjacent floors 
+        for (int i = 0; i < n; i++) {
+            cin >> T[i]; 
+        }
+
+        vector<vector<int>> elevators_floors(n);
+
+        cin.ignore(); // to ignore the newline character left in the input buffer
+
+        for (int j = 0; j < n; j++) {
+            string line;
+            getline(cin, line);
+            istringstream iss(line);
+            int number;
+            while (iss >> number) {
+                elevators_floors[j].push_back(number);
+            }
+        }
+
+        cout << n << " " << k << endl;
+
+        for (int i = 0; i < n; i++) {
+            cout << T[i] << " ";
+        }
+        cout << endl;
+
+        for (int i = 0; i < n; i++) {
+            for (int num : elevators_floors[i]) {
+                cout << num << " ";
+            }
+            cout << endl;
+        }
+
+        break;
+    }
+    return 0;
+}
