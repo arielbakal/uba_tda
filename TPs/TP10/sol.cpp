eb35015be8427e0550cc4f62b10c4572dd4d3fc2
arielbakal <arielbakal.dev@gmail.com>
2024-06-03 18:08:41 -0300

    TP10: sol.cpp build graph with nodes = (floor, elevator), hash funciton impl

--- TPs/TP10/sol.cpp
+++ TPs/TP10/sol.cpp
@@ -1,28 +1,56 @@
 #include <iostream>
 #include <sstream>
+#include <map>
 #include <vector>
 #include <unordered_map>
+#include <cstdlib>
 
 using namespace std;
+using node = pair<int, int>; // node = (floor, elevator);
 
 // NOTES: build graph and solve with dijkstra, this is sufficient for solving but could not solve in time. Start from there and go on.
 
 // 1. build graph as floor nodes with elevator states, and get costs between floors of same state.
 // 2. solve with dijkstra
 
-unordered_map<int, vector<int>> build_graph(vector<vector<int>> elevators_floors, int n) {
-    unordered_map<int, vector<int>> graph;
+struct hash_pair {
+    template <class T1, class T2>
+    size_t operator() (const pair<T1, T2>& p) const {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+        return hash1 ^ hash2;
+    }
+};
+
+map<node, map<node, int>> build_graph(vector<vector<int>> elevators_floors, vector<int> T, int n) {
+    map<node, map<node, int>> graph;
 
     for (int i=0; i<n; i++) {
-        int elevator_index = 100+i;
-        for (int floor: elevators_floors[i]) {
-            graph[floor].push_back(elevator_index);
-            graph[elevator_index].push_back(floor);
+        for (int floor1: elevators_floors[i]) {
+            for (int floor2: elevators_floors[i]) {
+                if (floor1 != floor2) {
+                    int cost = T[i] * abs(floor1 - floor2);
+                    graph[{floor1, i}][{floor2, i}] = cost;
+                    graph[{floor2, i}][{floor1, i}] = cost;
+                }
+            }
         }
     }
-
+    for (int i=0; i<n; i++) {
+        for (int j=0; i<n; i++) {
+            if (i != j) {
+                for (int floor1: elevators_floors[i]) {
+                    for (int floor2: elevators_floors[j]) {
+                        if (floor1 == floor2) {
+                            graph[{floor1, i}][{floor2, j}] = 60;
+                            graph[{floor2, j}][{floor1, i}] = 60;
+                        }
+                    }
+                }
+            }
+        }
+    }
     return graph;
-
 }
 
 
@@ -50,8 +78,14 @@ int main() {
                 elevators_floors[j].push_back(number);
             }
         }
+        map<node, map<node, int>> graph = build_graph(elevators_floors, T, n);
 
-        unordered_map<int, vector<int>> graph = build_graph(elevators_floors, n);
+        for (const auto& source : graph) {
+            std::cout << "Node (" << source.first.first << ", " << source.first.second << ") has edges:\n";
+            for (const auto& dest : source.second) {
+                std::cout << "  -> (" << dest.first.first << ", " << dest.first.second << ") with cost " << dest.second << "\n";
+            }
+        }
 
     }
     return 0;
