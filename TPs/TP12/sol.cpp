1a50aa6fc93237f5db2038e60973b6464029bfc6
arielbakal <arielbakal.dev@gmail.com>
2024-06-07 17:55:45 -0300

    TP12: sol.cpp dijkstra implemented

--- TPs/TP12/sol.cpp
+++ TPs/TP12/sol.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <climits>
 
 using namespace std;
+
+int dijkstra(const vector<vector<int>>& graph, int source, int target, int n) {
+    const int INF = INT_MAX;
+
+    vector<int> distances(n, INF);
+    queue<int> q;
+
+    distances[source] = 0;
+    q.push(source);
+
+    while (!q.empty()) {
+        int current = q.front();
+        q.pop();
+
+        // explore neighbors
+        for (int i = 0; i < n; i++) {
+            int neighbor_dist = graph[current][i];
+            if (neighbor_dist != 0) { // ensure there is an edge
+                int neighbor = i;
+                int new_dist = distances[current] + neighbor_dist;
+
+                // update distances
+                if (new_dist < distances[neighbor]) {
+                    distances[neighbor] = new_dist;
+                    q.push(neighbor);
+                }
+            }
+        }
+    }
+
+    return distances[target];
+}
 
 int main() {
     int t; cin >> t;
@@ -24,6 +58,8 @@ int main() {
         for (int i=0; i<n; i++) {
             cin >> tower_order[i];
         }
+
+        cout << dijkstra(power_matrix, 0, 1, n);
     }
     
     return 0;
