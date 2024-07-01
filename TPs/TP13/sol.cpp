3bc755e25f6ab3a633feeb5feb9265a95aead0d4
arielbakal <arielbakal.dev@gmail.com>
2024-07-01 00:14:46 -0300

    TP13: sol.cpp flow graph built correctly

--- TPs/TP13/sol.cpp
+++ TPs/TP13/sol.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
 using namespace std;
 
+// TODO: Impl Edmonds-Karp algorithm
+
 int size_index(string size) {
-    if (size == "XXL") return 0;
-    if (size == "XL") return 1;
-    if (size == "L") return 2;
-    if (size == "M") return 3;
-    if (size == "S") return 4;
-    if (size == "XS") return 5;
+    if (size == "XXL") return 1;
+    if (size == "XL") return 2;
+    if (size == "L") return 3;
+    if (size == "M") return 4;
+    if (size == "S") return 5;
+    if (size == "XS") return 6;
     return -1;
 }
 
@@ -20,22 +21,28 @@ int main() {
     for (int t=0; t<T; t++) {
 
         int N, M; cin >> N >> M;
-        vector<int> suiting_tshirts(6); // XXL=0, XL=1, ... , XS=5
-        vector<vector<pair<int,int>>> graph(8); 
+        vector<vector<pair<int,int>>> graph(M+9); 
+        // graph: 
+        // source->0, 
+        // XXL -> 1, XL -> 2, ... , XS -> 6
+        // volunteer_1 -> 7, volunteer_2 -> 1+7, ... , volunteer_M -> M+7
+        // sink->M+8
 
-        for (int m=0; m<M; m++) {
-
-            string size1, size2; cin >> size1 >> size2;
-            
-            suiting_tshirts[size_index(size1)] += 1;
-            suiting_tshirts[size_index(size2)] += 1;
-        }
-        
-        // build flow graph
-        for (int i=1; i<7; i++) { // reindex sizes i->i+1 to let i=0 to node s
-            // node s = 0th, node t = 7th
+        for (int i=1; i<7; i++) {
             graph[0].push_back({i, N/6}); // node s to all sizes
-            graph[i].push_back({7, suiting_tshirts[i-1]}); // node size to node t
+        }
+
+        int sink_node = M+8;
+        for (int m=0; m<M; m++) {
+            string size1, size2; cin >> size1 >> size2;
+            int volunteer_index = m+7;
+            
+            // direct two sizes that fits volunteer 
+            graph[size_index(size1)].push_back({volunteer_index, 1});
+            graph[size_index(size2)].push_back({volunteer_index, 1});
+
+            // direct volunteer to sink (each volunteer receives 1 t-shirt at most)
+            graph[volunteer_index].push_back({sink_node, 1});
         }
     }
     return 0;
