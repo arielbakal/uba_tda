4722e05aed406257c63f6c39c5ecdb6b7146e4d9
arielbakal <arielbakal.dev@gmail.com>
2024-07-02 09:02:09 -0300

    TP13: sol.cpp theorically finished

--- TPs/TP13/sol.cpp
+++ TPs/TP13/sol.cpp
@@ -1,9 +1,73 @@
 #include <iostream>
 #include <string>
+#include <limits.h>
+#include <queue>
 #include <vector>
 using namespace std;
 
-// TODO: Impl Edmonds-Karp algorithm
+bool bfs(vector<vector<pair<int, int>>> residual_graph, int s, int t, int parent[], int M) {
+
+    bool visited(M+9, false); 
+    queue<int> q;
+
+    // start from source
+    q.push(s);
+    visited[s] = true;
+    parent[s] = -1;
+
+    while(!q.empty()) {
+        int v = q.front();
+        q.pop();
+
+        for (int w = 0; w < M+9; w++) {
+            if (visited[w] == false && residual_graph[v][w] > 0) {
+                // if we reach sink just return true
+                if (w == t) {
+                    parent[w] = v;
+                    return true;
+                }
+                // continue exploring
+                q.push(w);
+                parent[w] = v;
+                visited[w] = true;
+            }   
+        }
+    }
+    // we didnt reach sink
+    return false;
+}
+
+int edmonds_karp(vector<vector<int>> graph, int s, int t, int M) {
+    int v, w;
+    
+    vector<vector<int>> residual_graph = graph;
+
+    vector<int> parent(M+9);
+
+    int max_flow = 0;
+
+    // augment flow while path source to sink exists
+    while (bfs(residual_graph, s, t, parent)) {
+        // find min residual capacity edges along the path filled by BFS 
+        // (find max flow through the path found)
+        int path_flow = INT_MAX;
+        for (int w=t; w!=s; w=parent[w]) {
+            v = parent[w];
+            path_flow = min(path_flow, residual_graph[v][w]);
+        }
+
+        // update residual capacities and reverse edges along the path
+        for (v = t; v != s; v = parent[v]) {
+            v = parent[w];
+            residual_graph[v][w] = path_flow;
+            residual_graph[w][v] = path_flow;
+        }
+
+        max_flow += path_flow;
+    }
+    return max_flow;
+}
+
 
 int size_index(string size) {
     if (size == "XXL") return 1;
@@ -21,7 +85,7 @@ int main() {
     for (int t=0; t<T; t++) {
 
         int N, M; cin >> N >> M;
-        vector<vector<pair<int,int>>> graph(M+9); 
+        vector<vector<int> graph(M+9, vector<int>(M+9, 0)); // space complexity O(n^2) but M<=30
         // graph: 
         // source->0, 
         // XXL -> 1, XL -> 2, ... , XS -> 6
@@ -29,7 +93,7 @@ int main() {
         // sink->M+8
 
         for (int i=1; i<7; i++) {
-            graph[0].push_back({i, N/6}); // node s to all sizes
+            graph[0][i] = N/6;
         }
 
         int sink_node = M+8;
@@ -38,12 +102,17 @@ int main() {
             int volunteer_index = m+7;
             
             // direct two sizes that fits volunteer 
-            graph[size_index(size1)].push_back({volunteer_index, 1});
-            graph[size_index(size2)].push_back({volunteer_index, 1});
+            graph[size_index(size1)][volunteer_index] = 1;
+            graph[size_index(size2)][volunteer_index] = 1;
 
             // direct volunteer to sink (each volunteer receives 1 t-shirt at most)
-            graph[volunteer_index].push_back({sink_node, 1});
+            graph[volunteer_index][sink_node] = 1;
         }
+
+        int result = edmonds_karp(graph, 0, M+8, M);
+
+        if (result = M) cout << "YES"; else cout << "NO";
+        cout << endl;
     }
     return 0;
 }
