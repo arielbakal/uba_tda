268a3fb7cf7cd3e06fac249791746709c71e27dc
arielbakal <arielbakal.dev@gmail.com>
2024-07-02 09:07:55 -0300

    TP13: sol.cpp edmonds_karp fixed and solves example inputs

--- TPs/TP13/sol.cpp
+++ TPs/TP13/sol.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 bool bfs(vector<vector<int>>& residual_graph, int s, int t, vector<int>& parent, int M) {
-
     vector<bool> visited(M+9, false); 
     queue<int> q;
 
@@ -20,7 +19,7 @@ bool bfs(vector<vector<int>>& residual_graph, int s, int t, vector<int>& parent,
         q.pop();
 
         for (int w = 0; w < M+9; w++) {
-            if (visited[w] == false && residual_graph[v][w] > 0) {
+            if (!visited[w] && residual_graph[v][w] > 0) {
                 // if we reach sink just return true
                 if (w == t) {
                     parent[w] = v;
@@ -33,15 +32,12 @@ bool bfs(vector<vector<int>>& residual_graph, int s, int t, vector<int>& parent,
             }   
         }
     }
-    // we didnt reach sink
+    // we didn't reach sink
     return false;
 }
 
 int edmonds_karp(vector<vector<int>> graph, int s, int t, int M) {
-    int v, w;
-    
     vector<vector<int>> residual_graph = graph;
-
     vector<int> parent(M+9);
 
     int max_flow = 0;
@@ -51,23 +47,22 @@ int edmonds_karp(vector<vector<int>> graph, int s, int t, int M) {
         // find min residual capacity edges along the path filled by BFS 
         // (find max flow through the path found)
         int path_flow = INT_MAX;
-        for (int w=t; w!=s; w=parent[w]) {
-            v = parent[w];
+        for (int w = t; w != s; w = parent[w]) {
+            int v = parent[w];
             path_flow = min(path_flow, residual_graph[v][w]);
         }
 
         // update residual capacities and reverse edges along the path
-        for (v = t; v != s; v = parent[v]) {
-            v = parent[w];
-            residual_graph[v][w] = path_flow;
-            residual_graph[w][v] = path_flow;
+        for (int w = t; w != s; w = parent[w]) {
+            int v = parent[w];
+            residual_graph[v][w] -= path_flow;
+            residual_graph[w][v] += path_flow;
         }
 
         max_flow += path_flow;
     }
     return max_flow;
 }
-
 
 int size_index(string size) {
     if (size == "XXL") return 1;
@@ -80,11 +75,11 @@ int size_index(string size) {
 }
 
 int main() {
-
-    int T; cin >> T;
-    for (int t=0; t<T; t++) {
-
-        int N, M; cin >> N >> M;
+    int T; 
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        int N, M; 
+        cin >> N >> M;
         vector<vector<int>> graph(M+9, vector<int>(M+9, 0)); // space complexity O(n^2) but M<=30
         // graph: 
         // source->0, 
@@ -92,14 +87,15 @@ int main() {
         // volunteer_1 -> 7, volunteer_2 -> 1+7, ... , volunteer_M -> M+7
         // sink->M+8
 
-        for (int i=1; i<7; i++) {
-            graph[0][i] = N/6;
+        for (int i = 1; i < 7; i++) {
+            graph[0][i] = N / 6;
         }
 
-        int sink_node = M+8;
-        for (int m=0; m<M; m++) {
-            string size1, size2; cin >> size1 >> size2;
-            int volunteer_index = m+7;
+        int sink_node = M + 8;
+        for (int m = 0; m < M; m++) {
+            string size1, size2; 
+            cin >> size1 >> size2;
+            int volunteer_index = m + 7;
             
             // direct two sizes that fits volunteer 
             graph[size_index(size1)][volunteer_index] = 1;
@@ -109,9 +105,10 @@ int main() {
             graph[volunteer_index][sink_node] = 1;
         }
 
-        int result = edmonds_karp(graph, 0, M+8, M);
+        int result = edmonds_karp(graph, 0, sink_node, M);
 
-        if (result = M) cout << "YES"; else cout << "NO";
+        if (result == M) cout << "YES"; 
+        else cout << "NO";
         cout << endl;
     }
     return 0;
