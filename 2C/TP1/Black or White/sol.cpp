455eb3de02bce246c7c51d41028bb32d8aa7167c
arielbakal <arielbakal.dev@gmail.com>
2024-09-17 15:29:15 -0300

    TP1-BW: solves correctly statements inputs

--- 2C/TP1/Black or White/sol.cpp
+++ 2C/TP1/Black or White/sol.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 // STATUS: Backtracking algo implemented
-// TODO: impl both asc and desc algorithm and test it; do recursive topdown;
+// TODO: do recursive topdown;
 
 int bt_incr(int i, int last_elem, const vector<int>& X) {
     
@@ -19,6 +19,17 @@ int bt_incr(int i, int last_elem, const vector<int>& X) {
     return max(bt_incr(i+1, X[i], X) + 1, bt_incr(i+1, last_elem, X)); 
 }
 
+int bt_decr(int i, int last_elem, const vector<int>& X) {
+    
+    if (i==X.size()) {
+        return 0;
+    }
+    if (last_elem <= X[i]){ // Si NO es increasing, continuo
+        return bt_decr(i+1, last_elem, X);
+    }   
+    // Si es increasing, busco el maximo entre actualizar last_elem y continuar explorando
+    return max(bt_decr(i+1, X[i], X) + 1, bt_decr(i+1, last_elem, X)); 
+}
 
 int main()
 {
@@ -31,10 +42,15 @@ int main()
         }
         
         int res_incr;
+        int res_decr;
         
         res_incr = bt_incr(0, INT_MIN, X);
+        res_decr = bt_decr(0, INT_MAX, X);
         
-        cout << res_incr << endl;
+        int res;
+        res = N - res_incr - res_decr;
+        
+        cout << res << endl;
     }
     
     return 0;
