74e3a55f79f9c2f716fff77fecf6329cb3017a1b
arielbakal <arielbakal.dev@gmail.com>
2024-09-26 12:28:55 -0300

    TP1-BORW: passes the judge!

--- 2C/TP1/Black or White/sol.cpp
+++ 2C/TP1/Black or White/sol.cpp
@@ -4,28 +4,34 @@
 #include <limits.h>
 using namespace std;
 
-// STATUS: bt impl
+// STATUS: topdown impl
 // TODO: funciona para todos los tests que conozco
 
-int bt_incr_decr(int i, int black, int white, const vector<int>& X, const int N) {
+int topdown_incr_decr(int i, int black, int white, vector<vector<vector<int>>>& Memo, const vector<int>& X, const int N) {
     
     if (i==N) {
         return 0;
     }
     
+    if (Memo[i][black][white] != -1) {
+        return Memo[i][black][white]; 
+    }
+    
     int include_black = INT_MIN;
     int include_white = INT_MIN;
     
-    if (black == -1 || X[black] < X[i]) {
-        include_black = bt_incr_decr(i+1, i, white, X, N) + 1;
+    if (black == N || X[black] < X[i]) {
+        include_black = topdown_incr_decr(i+1, i, white, Memo, X, N) + 1;
     }
     
-    if (white == -1 || X[white] > X[i]) {
-        include_white = bt_incr_decr(i+1, black, i, X, N) + 1;
+    if (white == N || X[white] > X[i]) {
+        include_white = topdown_incr_decr(i+1, black, i, Memo, X, N) + 1;
     } 
-    int exclude = bt_incr_decr(i+1, black, white, X, N);
+    int exclude = topdown_incr_decr(i+1, black, white, Memo, X, N);
     
-    return max(max(include_black, include_white), exclude);
+    Memo[i][black][white] = max(max(include_black, include_white), exclude);
+    
+    return Memo[i][black][white];
 }
 
 int main() {
@@ -35,9 +41,11 @@ int main() {
         vector<int> X(N);
         for (int i = 0; i < N; i++) {
             cin >> X[i];
-        } 
+        }
+        
+        vector<vector<vector<int>>> Memo(N, vector<vector<int>>(N+1, vector<int>(N+1, -1)));
 
-        int res = bt_incr_decr(0, -1, -1, X, N);
+        int res = topdown_incr_decr(0, N, N, Memo, X, N);
         
         cout << N - res << endl;
     }
