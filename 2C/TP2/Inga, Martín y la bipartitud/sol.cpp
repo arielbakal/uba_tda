d9e2ab11a7be91208dc5899de521f8ef6228f21d
arielbakal <arielbakal.dev@gmail.com>
2024-10-19 16:45:00 -0300

    TP2: B sol.cpp optimized but still TLE

--- 2C/TP2/Inga, Martín y la bipartitud/sol.cpp
+++ 2C/TP2/Inga, Martín y la bipartitud/sol.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-vector<bool> graph_coloring_dfs(const vector<vector<int>> graph, vector<bool>& visited, vector<bool>& colors, int u) {
+void graph_coloring_dfs(const vector<vector<int>> graph, int& elems_in_set_A, vector<bool>& visited, vector<bool>& colors, int u) {
 
     visited[u] = true;
 
     for (int v : graph[u]) {
         if (!visited[v]) {
-            if (colors[u] == 0) {colors[v] = 1;} else {colors[v] = 0;}
-            graph_coloring_dfs(graph, visited, colors, v);
+            if (colors[u] == 0) {colors[v] = 1;} else {colors[v] = 0; elems_in_set_A += 1;}
+            graph_coloring_dfs(graph, elems_in_set_A, visited, colors, v);
         }
     }
-
-    return colors;
 }
 
 int main() {
     int n; cin >> n; 
 
     vector<vector<int>> graph(n);
-    vector<bool> visited(n);
-    vector<bool> assigned_edges(n);
-    vector<bool> empty_colors(n);
+    vector<bool> visited(n, false);
+    vector<bool> assigned_edges(n, false);
+    vector<bool> colors(n, false);
 
     for (int i=0; i<n-1; i++) { // we read n-1 edges
         int u, v; cin >> u >> v;
@@ -32,14 +29,9 @@ int main() {
     }
 
     // get colored nodes
-    empty_colors[0] = 0; // root node has color 0
-    vector<bool> colors = graph_coloring_dfs(graph, visited, empty_colors, 0);
-
-    int elems_in_set_A = 0;
-
-    for (int v=0; v<n; v++) {
-        if (colors[v]) {elems_in_set_A += 1;}
-    }
+    colors[0] = 0; // root node has color 0
+    int elems_in_set_A = 1;
+    graph_coloring_dfs(graph, elems_in_set_A, visited, colors, 0);
 
     // count edges
     int res = (elems_in_set_A * (n - elems_in_set_A)) - (n-1);
