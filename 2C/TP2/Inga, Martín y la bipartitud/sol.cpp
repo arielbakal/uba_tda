c9a5a7cb0314dbbc73e8ab094c844371ba73aa50
arielbakal <arielbakal.dev@gmail.com>
2024-10-19 16:41:32 -0300

    TP2: B sol.cpp TLE but solves

--- 2C/TP2/Inga, Martín y la bipartitud/sol.cpp
+++ 2C/TP2/Inga, Martín y la bipartitud/sol.cpp
@@ -3,14 +3,6 @@
 #include <algorithm>
 using namespace std;
 
-// We've got a tree graph with n nodes and n-1 edges.
-// To avoid creating empty spaces in graph, the node indexation starts at 0 (each input node should be substracted by 1)
-// Idea:
-// 1. Color tree nodes in a way that adjacent nodes have diff colors. This is bc if we add backward edges that connects
-// same color nodes, we create an odd cycle => that edge makes the graph non-bipartite. 
-// To do so, we traverse the graph with DFS while coloring nodes.
-// 2. Count edges that keep the graph bipartite. For each node we have n(n-1)/2 - (n-1)  ...
-
 vector<bool> graph_coloring_dfs(const vector<vector<int>> graph, vector<bool>& visited, vector<bool>& colors, int u) {
 
     visited[u] = true;
@@ -23,26 +15,6 @@ vector<bool> graph_coloring_dfs(const vector<vector<int>> graph, vector<bool>& v
     }
 
     return colors;
-}
-
-int count_non_bipartite_edges(const vector<vector<int>> graph, const int n, vector<bool>& assigned_edges, vector<bool>& colors) {
-
-    int count = 0;
-
-    for (int u=0; u<n; u++) {
-        assigned_edges[u] = true;
-        
-        for (int v=0; v<n; v++) {
-            if (v != u && !(find(graph[u].begin(), graph[u].end(), v) == graph[u].end())) { // if v != u and v not a neighbor of u
-                if (!assigned_edges[v]) {
-                    if (colors[v] != colors[u]) { // we have an even cycle => we still have a bipartite graph
-                        count += 1;
-                    }
-                }
-            }
-        }
-    }
-    return count;
 }
 
 int main() {
@@ -63,8 +35,14 @@ int main() {
     empty_colors[0] = 0; // root node has color 0
     vector<bool> colors = graph_coloring_dfs(graph, visited, empty_colors, 0);
 
+    int elems_in_set_A = 0;
+
+    for (int v=0; v<n; v++) {
+        if (colors[v]) {elems_in_set_A += 1;}
+    }
+
     // count edges
-    int res = count_non_bipartite_edges(graph, n, assigned_edges, colors);
+    int res = (elems_in_set_A * (n - elems_in_set_A)) - (n-1);
 
     cout << res << endl;
 
