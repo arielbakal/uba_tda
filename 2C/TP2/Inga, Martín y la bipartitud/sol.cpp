1b41a5420dc2f96b43f982b7e50844e5f1b8496c
arielbakal <arielbakal.dev@gmail.com>
2024-10-19 16:19:35 -0300

    TP2: B sol.cpp impl but wrong outputs

--- 2C/TP2/Inga, Martín y la bipartitud/sol.cpp
+++ 2C/TP2/Inga, Martín y la bipartitud/sol.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <algorithm>
 using namespace std;
 
 // We've got a tree graph with n nodes and n-1 edges.
 // To avoid creating empty spaces in graph, the node indexation starts at 0 (each input node should be substracted by 1)
+// Idea:
+// 1. Color tree nodes in a way that adjacent nodes have diff colors. This is bc if we add backward edges that connects
+// same color nodes, we create an odd cycle => that edge makes the graph non-bipartite. 
+// To do so, we traverse the graph with DFS while coloring nodes.
+// 2. Count edges that keep the graph bipartite. For each node we have n(n-1)/2 - (n-1)  ...
 
 vector<bool> graph_coloring_dfs(const vector<vector<int>> graph, vector<bool>& visited, vector<bool>& colors, int u) {
 
@@ -20,17 +25,32 @@ vector<bool> graph_coloring_dfs(const vector<vector<int>> graph, vector<bool>& v
     return colors;
 }
 
-int count_non_bipartite_edges(vector<vector<int>> graph) {
+int count_non_bipartite_edges(const vector<vector<int>> graph, const int n, vector<bool>& assigned_edges, vector<bool>& colors) {
 
-    return 0;
+    int count = 0;
+
+    for (int u=0; u<n; u++) {
+        assigned_edges[u] = true;
+        
+        for (int v=0; v<n; v++) {
+            if (v != u && !(find(graph[u].begin(), graph[u].end(), v) == graph[u].end())) { // if v != u and v not a neighbor of u
+                if (!assigned_edges[v]) {
+                    if (colors[v] != colors[u]) { // we have an even cycle => we still have a bipartite graph
+                        count += 1;
+                    }
+                }
+            }
+        }
+    }
+    return count;
 }
-
 
 int main() {
     int n; cin >> n; 
 
     vector<vector<int>> graph(n);
     vector<bool> visited(n);
+    vector<bool> assigned_edges(n);
     vector<bool> empty_colors(n);
 
     for (int i=0; i<n-1; i++) { // we read n-1 edges
@@ -42,6 +62,11 @@ int main() {
     // get colored nodes
     empty_colors[0] = 0; // root node has color 0
     vector<bool> colors = graph_coloring_dfs(graph, visited, empty_colors, 0);
+
+    // count edges
+    int res = count_non_bipartite_edges(graph, n, assigned_edges, colors);
+
+    cout << res << endl;
 
     return 0;
 }
