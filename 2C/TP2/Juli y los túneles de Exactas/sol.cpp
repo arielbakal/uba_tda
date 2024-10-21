5e5e25ef286c2434503bf81dc575c4b6616486df
arielbakal <arielbakal.dev@gmail.com>
2024-10-21 17:08:56 -0300

    TP2C: sol.cpp accepted!

--- 2C/TP2/Juli y los túneles de Exactas/sol.cpp
+++ 2C/TP2/Juli y los túneles de Exactas/sol.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
+
+void bfs_coloring(vector<vector<int>>& graph, const int n, vector<bool>& visited, vector<int>& distance_from_0, int u) {
+    queue<int> q;
+    q.push(u);
+    visited[u] = true;
+    distance_from_0[u] = 0;
+
+    while (!q.empty()) {
+        int f = q.front();
+        q.pop();
+
+        for (int v: graph[f]) {
+            if (!visited[v]) {
+                q.push(v);
+                visited[v] = true;
+                distance_from_0[v] = distance_from_0[f] + 1;
+            }
+        }
+    }
+}
 
 int main() {
 
     int n; cin >> n;
-
     vector<vector<int>> graph(n);
+    vector<bool> visited(n, false);
+    vector<int> distance_from_0(n, -1);
 
     for (int i=0; i<n; i++) {
-
         // build directed graph
         if (i==0) {
             graph[i].push_back(i+1);        
@@ -27,17 +48,10 @@ int main() {
             graph[i].push_back(shortcut_to_class-1);    
         }
     }
+    bfs_coloring(graph, n, visited, distance_from_0, 0);
 
     for (int i=0; i<n; i++) {
-
-        cout << i << ": ";
-
-        for (int v: graph[i]) {
-
-            cout << v << " ";
-        }
-
-        cout << endl;
+        cout << distance_from_0[i] << " ";
     }
 
     return 0;
