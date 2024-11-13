c50462f091d0b507d5a22823667274629143081f
Ariel Bakal <arielbakal.dev@gmail.com>
2024-11-12 21:07:30 -0300

    TP3C: sol.cpp solves non multigraph

---
+++ 2C/TP3/Seguridad/sol.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <utility> 
+#include <limits>
+
+using namespace std;
+
+long long dijkstra(const vector<vector<pair<int, int>>>& graph, int n, int source, int target) {
+    int INF = numeric_limits<int>::max();
+    
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    vector<long long> dist(n, INF);
+    dist[source] = 0;
+    pq.push({0, source});
+
+    while (!pq.empty()) {
+        int d = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
+
+        // Explore neighbors
+        for (const auto &neighbor : graph[u]) {
+            int v = neighbor.first;
+            int weight = neighbor.second;
+
+            // If a shorter path to v is found
+            if (dist[v] > dist[u] + weight) {
+                dist[v] = dist[u] + weight;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+    return dist[n-1];
+}
+
+int main() {
+    int n, m; cin >> n >> m;
+    vector<vector<pair<int, int>>> graph(n);
+
+    for (int i=0; i<n; i++) {
+        int v, w, c; cin >> v >> w >> c;
+        // build adj list O(m+n)
+        if (v!=w) { // avoid loop edges
+            // saving pair<w, c> as a neighbor of v ensures multi-edges handling bc they will be unique in that way
+            // i can easily access c(v,w) also
+            graph[v].emplace_back(w, c);
+            graph[w].emplace_back(v, c);
+        }
+    }
+    // dijkstra impl with binary heap => O((m+n)logn) = O(m.log(n)) since m > n
+    long long min_dist = dijkstra(graph, n, 0, n-1);
+
+    // return 2*c_0 + 2*c_k + ... + 2*c_n-1 = 2*Σ(c_p) = 2*(shortest path between 0 and n-1) where 0<k<n-1
+    // and k represents the nodes idx from the shortest path between 0 and n-1, then p={0,k....,n-1}
+    cout << 2*min_dist;
+
+    return 0;
+}
