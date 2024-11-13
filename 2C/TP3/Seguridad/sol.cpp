e944074b307cb21a77f86a7c01d8d2aa4498b5d3
Ariel Bakal <arielbakal.dev@gmail.com>
2024-11-13 12:04:47 -0300

    TP3C: sol.cpp solves til test 3

--- 2C/TP3/Seguridad/sol.cpp
+++ 2C/TP3/Seguridad/sol.cpp
@@ -6,9 +6,10 @@
 
 using namespace std;
 
-long long dijkstra(const vector<vector<pair<int, int>>>& graph, int n, int source, int target) {
+long long dijkstra(const vector<vector<pair<int, int>>>& graph, int n, int source) {
     int INF = numeric_limits<int>::max();
-    
+    int diff_shortest_paths = 0;
+
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     vector<long long> dist(n, INF);
     dist[source] = 0;
@@ -19,26 +20,36 @@ long long dijkstra(const vector<vector<pair<int, int>>>& graph, int n, int sourc
         int u = pq.top().second;
         pq.pop();
 
+        if (d > dist[u]) continue;
+
         // Explore neighbors
         for (const auto &neighbor : graph[u]) {
             int v = neighbor.first;
             int weight = neighbor.second;
-
+            
             // If a shorter path to v is found
             if (dist[v] > dist[u] + weight) {
                 dist[v] = dist[u] + weight;
                 pq.push({dist[v], v});
-            }
+            } 
         }
     }
-    return dist[n-1];
+    
+    // if dist[neighbor] + c == dist[n-1] that means neighbor belogs to a shortest path to n-1
+    for (const auto& [u, c] : graph[n-1]) {
+        if (dist[n-1] == dist[u] + c) {
+            diff_shortest_paths++;
+        }
+    }
+    
+    return diff_shortest_paths*dist[n-1];
 }
 
 int main() {
     int n, m; cin >> n >> m;
     vector<vector<pair<int, int>>> graph(n);
 
-    for (int i=0; i<n; i++) {
+    for (int i=0; i<m; i++) {
         int v, w, c; cin >> v >> w >> c;
         // build adj list O(m+n)
         if (v!=w) { // avoid loop edges
@@ -49,11 +60,11 @@ int main() {
         }
     }
     // dijkstra impl with binary heap => O((m+n)logn) = O(m.log(n)) since m > n
-    long long min_dist = dijkstra(graph, n, 0, n-1);
+    long long all_min_dist = dijkstra(graph, n, 0);
 
     // return 2*c_0 + 2*c_k + ... + 2*c_n-1 = 2*Σ(c_p) = 2*(shortest path between 0 and n-1) where 0<k<n-1
     // and k represents the nodes idx from the shortest path between 0 and n-1, then p={0,k....,n-1}
-    cout << 2*min_dist;
+    cout << 2*all_min_dist;
 
     return 0;
 }
