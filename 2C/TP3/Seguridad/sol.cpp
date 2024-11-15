eb80317b228404272ad6db70ee975c8cd9729284
arielbakal <arielbakal.dev@gmail.com>
2024-11-14 23:44:01 -0300

    TP3C: sol.cpp accepted!

--- 2C/TP3/Seguridad/sol.cpp
+++ 2C/TP3/Seguridad/sol.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <utility> 
 #include <limits>
-#include <tuple>
 
 using namespace std;
 
@@ -22,13 +21,11 @@ vector<long long> dijkstra(const vector<vector<pair<int, int>>>& graph, int n, i
         pq.pop();
 
         if (d > dist[u]) continue;
-
-        // Explore neighbors
+        
         for (const auto &neighbor : graph[u]) {
             int v = neighbor.first;
             int weight = neighbor.second;
             
-            // If a shorter path to v is found
             if (dist[v] > dist[u] + weight) {
                 dist[v] = dist[u] + weight;
                 pq.push({dist[v], v});
@@ -50,8 +47,11 @@ void dfs(int u, int n, vector<bool>& visited, const vector<vector<pair<int, int>
                     dfs(v, n, visited, graph, min_dist, total_cost);
                 }
             }
-        }   
-        
+        } else {
+            if (min_dist[u] == min_dist[v] + c) {
+                total_cost += c;
+            }
+        } 
     }
 }
 
@@ -69,15 +69,22 @@ int main() {
             graph[w].emplace_back(v, c);
         }
     }
-    // dijkstra impl with binary heap => O((m+n)logn) = O(m.log(n)) since m > n
+    // dijkstra impl with binary heap => O((m+n)logn) = O(m.log(n)) since m > n 
     vector<long long> all_min_dist = dijkstra(graph, n, 0);
 
     long long total_cost = 0;
-
     vector<bool> visited(n);
+    
+    // dfs from node n-1 to only nodes from all 0 to (n-1) shortest path in o(m+n)
+    // traversing only nodes that satisfies min_dist[u] == min_dist[v] + c can achieve that
+    // to handle multiple edges, if it satisfies the condition then we have equal
+    // multiple edges that we need to sum to total_cost also (bc they also belong to a shortest path)
     dfs(n-1, n, visited, graph, all_min_dist, total_cost);
 
     cout << total_cost*2 << endl;
+    
+    // complexity = O((m+n)log(n) + 2(m+n) + 2n) = O(m.log(n))
+    //            = (dijkstra + bfs) + (adj_list + visited + dist)
 
     return 0;
 }
